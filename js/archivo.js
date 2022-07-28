3b1e2bbc5a03ad8b6d3bd2ad775a35da571b6c28
Juan Esteban Alzate <juanealzate800@gmail.com>
2022-07-28 17:05:21 -0500

    modificando

--- js/archivo.js
+++ js/archivo.js
@@ -1,7 +1,5 @@
 let promocion = alert('Envios gratis por compras superiores a $ 100.000')
 
-
-
 // creo  una clase  productos donde  donde abarco la tienda de productos
 class Producto{
     constructor(nombre, precio, id){
@@ -25,85 +23,78 @@ productos.push(new Producto(' te meishen', '29000', 7));
 productos.push(new Producto('levadura', '40000', 8));
 
 for(const producto of productos ){
-    producto.descuento()
+    producto.descuento();
 }
-// console.log(productos)
+console.log(productos);
 
-// let pasar = parseInt(prompt('ingrese el id de tu producto y si no desea alguno de nuestros producto ingresar no compara'));
+let pasar = parseInt(prompt('ingrese el id de tu producto y si no desea alguno de nuestros producto ingresar no compara'));
 
-// function mostrar(senal){
-//     alert(senal)
-// }
-
-// while(pasar != 'no comprar'){
-// if(pasar === 1){
-//     productos.find((el) => {
-//         if(el.id === 1){
-//             mostrar( el.nombre + ' ' + el.precio);
-//         }
-//     })
-// } else if(pasar === 2){
-//     productos.find((el) => {
-//         if(el.id === 2){
-//             mostrar( el.nombre + ' ' + el.precio);
-//         }
-//     })
-// } else if(pasar === 3){
-//     productos.find((el) => {
-//         if(el.id === 3){
-//             mostrar( el.nombre + ' ' + el.precio);
-//         }
-//     })
-// } else if(pasar === 4){
-//     productos.find((el) => {
-//         if(el.id === 4){
-//             mostrar( el.nombre + ' ' + el.precio);
-//         }
-//     })
-// } else if(pasar === 5){
-//     productos.find((el) => {
-//         if(el.id === 5){
-//             mostrar( el.nombre + ' ' + el.precio);
-//         }
-//     })
-// } else if(pasar === 6){
-//     productos.find((el) => {
-//         if(el.id === 6){
-//             mostrar( el.nombre + ' ' + el.precio);
-//         }
-//     })
-// } else if(pasar === 7){
-//     productos.find((el) => {
-//         if(el.id === 7){
-//             mostrar( el.nombre + ' ' + el.precio);
-//         }
-//     })
-// } else if(pasar === 8){
-//     productos.find((el) => {
-//         if(el.id === 8){
-//             mostrar( el.nombre + ' ' +  el.precio);
-//         }
-//     })
-// }else if(pasar != 1 ||2 ||3 ||4 ||5 ||6 ||7 ||8  ){
-//     mostrar('Los sentimos por no cumplir con tu deseo de compra')
-// }
-// pasar = parseInt(prompt('ingrese el id de tu producto y si no desea alguno de nuestros producto ingresar no compara'));
-// }
-
-let entrada = parseInt(prompt('ingresa producto a comprar'));
-
-let carrito = [];
-
-const productoEncontrado = productos.find(el => el.id === entrada);
-while(entrada != 'no agregar'){
-
-    if(productoEncontrado){
-        carrito.push(productoEncontrado);
-    }else{
-        console.log('producto no encontrado');
-    }
-    entrada = parseInt(prompt('ingrese producto a comprar'))
+function mostrar(senal){
+    alert(senal)
 }
-alert('tu compra realiza fue por un valor' + '')
-//
-// 
+
+while(pasar != 'no comprar'){
+if(pasar === 1){
+    productos.find((el) => {
+        if(el.id === 1){
+            mostrar( el.nombre + ' ' + el.precio);
+        }
+    })
+} else if(pasar === 2){
+    productos.find((el) => {
+        if(el.id === 2){
+            mostrar( el.nombre + ' ' + el.precio);
+        }
+    })
+} else if(pasar === 3){
+    productos.find((el) => {
+        if(el.id === 3){
+            mostrar( el.nombre + ' ' + el.precio);
+        }
+    })
+} else if(pasar === 4){
+    productos.find((el) => {
+        if(el.id === 4){
+            mostrar( el.nombre + ' ' + el.precio);
+        }
+    })
+} else if(pasar === 5){
+    productos.find((el) => {
+        if(el.id === 5){
+            mostrar( el.nombre + ' ' + el.precio);
+        }
+    })
+} else if(pasar === 6){
+    productos.find((el) => {
+        if(el.id === 6){
+            mostrar( el.nombre + ' ' + el.precio);
+        }
+    })
+} else if(pasar === 7){
+    productos.find((el) => {
+        if(el.id === 7){
+            mostrar( el.nombre + ' ' + el.precio);
+        }
+    })
+} else if(pasar === 8){
+    productos.find((el) => {
+        if(el.id === 8){
+            mostrar( el.nombre + ' ' +  el.precio);
+        }
+    })
+}else if(pasar != 1 ||2 ||3 ||4 ||5 ||6 ||7 ||8  ){
+    mostrar('Los sentimos por no cumplir con tu deseo de compra');
+}
+pasar = parseInt(prompt('ingrese el id de tu producto y si no desea alguno de nuestros producto ingresar no compara'));
+}
+
+// let entrada = parseInt(prompt('ingresa producto a comprar'));
+
+// let carrito = [];
+// const productoEncontrado = productos.find(el => el.id === entrada);    
+// if(productoEncontrado){
+//     carrito.push(productoEncontrado);
+// }else{
+//     console.log('producto no encontrado');
+// }
+// console.log(carrito);
